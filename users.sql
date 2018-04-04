d421327c55775ff9dc1838484fbf0bdfb28cd430
Cassiexyq <Cassiexuan_yq@163.com>
2018-04-04 23:17:38 +0800

    数据库代码

---
+++ users.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` VALUES (7, 'aa', 'a123456', 'MAN', NULL);
+INSERT INTO `users` VALUES (8, 'bb', 'b123456', 'WOMAN', NULL);
+INSERT INTO `users` VALUES (9, 'cc', 'b123456', 'WOMAN', 'neo');
+INSERT INTO `users` VALUES (10, 'aa', 'a123456', 'MAN', NULL);
+INSERT INTO `users` VALUES (11, 'bb', 'b123456', 'WOMAN', NULL);
+INSERT INTO `users` VALUES (12, 'cc', 'b123456', 'WOMAN', NULL);
